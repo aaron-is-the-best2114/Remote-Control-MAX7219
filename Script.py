8c4ab0b53595197f11173479acec52820e8737f6
Aaron <85415917+aaron-is-the-best2114@users.noreply.github.com>
2023-07-05 09:15:07 -0400

    Update Script.py

--- Script.py
+++ Script.py
@@ -32,13 +32,13 @@ def draw_dot():
     with canvas(device) as draw:
         draw.point((x, y), fill="white")
 
-#draw_dot()
-
 # Move dot based on arrow key press
 for event in ir_device.read_loop():
     if event.type == evdev.ecodes.EV_MSC and event.code == evdev.ecodes.MSC_SCAN:
+        
         #You may have to change the values based on the type of remote you have.
         #Run IR_Sensor_Reader.py to see the correct values for your remote
+        
         if event.value == 1088: #Up Arrow
             y = max(0, y - 1)
         elif event.value == 1089: #Down Arrow
@@ -52,42 +52,43 @@ for event in ir_device.read_loop():
         #If you have numbers on your remote. You may have to adjust the values accrodingly
         #Run IR_Sensor_Reader.py to see the correct values for your remote
         #To add numbers to the Display get rid of the quotation marks around the code below
+        
         '''
-        if event.value == 1041:
+        if event.value == 1041: #1 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "1", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1042:
+        if event.value == 1042: #2 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "2", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1043:
+        if event.value == 1043: #3 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "3", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1044:
+        if event.value == 1044: #4 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "4", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1045:
+        if event.value == 1045: #5 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "5", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1046:
+        if event.value == 1046: #6 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "6", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1047:
+        if event.value == 1047: #7 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "7", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1048:
+        if event.value == 1048: #8 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "8", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1049:
+        if event.value == 1049: #9 Button
             with canvas(device) as draw:
                 text(draw, (1, 0), "9", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1040:
+        if event.value == 1040: #0 Button
             with canvas(device) as draw:
                 text(draw, (0, 0), "0", fill="white", font=proportional(CP437_FONT))
-        if event.value == 1032:
+        if event.value == 1032: #On/Off Button
             with canvas(device) as draw:
                 text(draw, (0, 0), "-", fill="black", font=proportional(CP437_FONT))
                 continue
                 
         '''
-        print(event.value)
+        print(event.value) #Printing the value codes from the remote to the console
         
